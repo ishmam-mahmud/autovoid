1b8c705216cee5c5ccbd41c5b1c2cd5ca71b5de7
Ishmam Mahmud <mdishmam1999@gmail.com>
2023-06-06 22:59:33 +0800

    Limit API calls

--- lib/autovoid/channel.ex
+++ lib/autovoid/channel.ex
@@ -17,7 +17,13 @@ defmodule Autovoid.Channel do
 
   @spec add_message_ids(t(), list(pos_integer())) :: t()
   def add_message_ids(%__MODULE__{} = channel, message_ids) when is_list(message_ids) do
-    Map.update(channel, :message_ids, message_ids, fn existing -> existing ++ message_ids end)
+    Map.update(channel, :message_ids, message_ids, fn existing ->
+      Enum.uniq(existing ++ message_ids)
+    end)
+  end
+
+  def get_number_of_messages(%__MODULE__{} = channel) do
+    length(channel.message_ids)
   end
 
   @spec clear_message_ids(t()) :: t()
--- lib/autovoid/channel_session.ex
+++ lib/autovoid/channel_session.ex
@@ -3,7 +3,7 @@ defmodule Autovoid.ChannelSession do
 
   alias Autovoid.Channel
 
-  @interval 1000
+  @interval 2000
 
   def start_link(channel_id) when is_integer(channel_id) do
     GenServer.start_link(__MODULE__, channel_id, name: global_name(channel_id))
@@ -17,8 +17,7 @@ defmodule Autovoid.ChannelSession do
 
   @impl GenServer
   def handle_info(:get_messages, %Channel{} = channel) do
-    IO.inspect(channel)
-    messages = Nostrum.Api.get_channel_messages!(channel.id, 2) |> IO.inspect()
+    messages = Nostrum.Api.get_channel_messages!(channel.id, 2)
     message_ids = Enum.map(messages, fn %Nostrum.Struct.Message{} = message -> message.id end)
     updated_channel = Channel.add_message_ids(channel, message_ids)
     Process.send_after(self(), :delete_messages, @interval)
@@ -27,9 +26,15 @@ defmodule Autovoid.ChannelSession do
 
   @impl GenServer
   def handle_info(:delete_messages, %Channel{} = channel) do
-    {:ok} = Nostrum.Api.bulk_delete_messages!(channel.id, channel.message_ids)
+    updated_channel =
+      case Channel.get_number_of_messages(channel) do
+        num_messages when num_messages >= 2 ->
+          {:ok} = Nostrum.Api.bulk_delete_messages!(channel.id, channel.message_ids)
+          Channel.clear_message_ids(channel)
 
-    updated_channel = Channel.clear_message_ids(channel)
+        _other ->
+          channel
+      end
 
     Process.send_after(self(), :get_messages, @interval)
 
