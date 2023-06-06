ab0f6d5535c1e238579cb66aa08ccf643280216a
Ishmam Mahmud <mdishmam1999@gmail.com>
2023-06-06 23:25:35 +0800

    Apply API limits

--- lib/autovoid/channel_session.ex
+++ lib/autovoid/channel_session.ex
@@ -4,6 +4,8 @@ defmodule Autovoid.ChannelSession do
   alias Autovoid.Channel
 
   @interval 60_000
+  @get_messages_limit 100
+  @min_messages_delete 2
 
   def start_link(channel_id) when is_integer(channel_id) do
     GenServer.start_link(__MODULE__, channel_id, name: global_name(channel_id))
@@ -17,7 +19,7 @@ defmodule Autovoid.ChannelSession do
 
   @impl GenServer
   def handle_info(:get_messages, %Channel{} = channel) do
-    messages = Nostrum.Api.get_channel_messages!(channel.id, :infinity)
+    messages = Nostrum.Api.get_channel_messages!(channel.id, @get_messages_limit)
     message_ids = Enum.map(messages, fn %Nostrum.Struct.Message{} = message -> message.id end)
     updated_channel = Channel.add_message_ids(channel, message_ids)
     Process.send_after(self(), :delete_messages, @interval)
@@ -28,7 +30,7 @@ defmodule Autovoid.ChannelSession do
   def handle_info(:delete_messages, %Channel{} = channel) do
     updated_channel =
       case Channel.get_number_of_messages(channel) do
-        num_messages when num_messages >= 2 ->
+        num_messages when num_messages >= @min_messages_delete ->
           {:ok} = Nostrum.Api.bulk_delete_messages!(channel.id, channel.message_ids)
           Channel.clear_message_ids(channel)
 
